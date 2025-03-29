3f886fee081b5659f0614f2a8c0fabd832a9a9d2
mohammadhasanmehrabi <mhm.l.m.478155@gmail.com>
2025-03-29 06:15:45 +0330

    new

---
+++ MyPersonalBlog/app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from config import Config
+from .models import db
+from .controllers.main import main_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+    app.register_blueprint(main_bp)
+
+    return app
