c84ec1e101a9812d4ebe5458b440a6b90d5c0a0f
mohammadhasanmehrabi <mhm.l.m.478155@gmail.com>
2025-03-29 04:19:25 +0330

    دد

--- MyPersonalBlog/app/__init__.py
+++
@@ -1,2 +0,0 @@
-from .controllers.main import main_bp
-app.register_blueprint(main_bp)
--- MyPersonalBlog/app/controllers/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from ..models import User, Post
-
-main_bp = Blueprint('main', __name__)
-
-@main_bp.route('/')
-def index():
-    posts = Post.query.all()
-    return render_template('index.html', posts=posts)
