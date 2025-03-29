d8a165cd1dc678096f1d600fb44f47bb2728a529
mohammadhasanmehrabi <mhm.l.m.478155@gmail.com>
2025-03-29 07:03:00 +0330

    ب

--- MyPersonalBlog/app/controllers/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from app.models import Post
-
-main_bp = Blueprint('main', __name__)
-
-@main_bp.route('/')
-def index():
-    posts = Post.query.all()
-    return render_template('index.html', posts=posts)
--- MyPersonalBlog/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('134713811355') 
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:134713811355@localhost/blog'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
